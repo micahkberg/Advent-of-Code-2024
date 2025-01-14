81af64cc3b3dfc876d2428519550124666a91d45
nmpcgithub <micahkberg@gmail.com>
2025-01-14 14:28:39 -0800

    day 1 done

--- 01.py
+++ 01.py
@@ -10,6 +10,20 @@ def read_input(fname, strip=True):
 
 lines = read_input("01.txt")
 nums = [i.split() for i in lines]
-l_nums = [int(i[0]) for i in nums]
-r_nums = [int(i[0]) for i in nums]
-print(l_nums)
+l_nums = sorted([int(i[0]) for i in nums])
+r_nums = sorted([int(i[1]) for i in nums])
+distance_total = 0
+for i in range(len(l_nums)):
+    l = l_nums[i]
+    r = r_nums[i]
+    distance_total += abs(l-r)
+print(f"Part 1: {distance_total}")
+
+similarity_total=0
+for l in l_nums:
+    l_count = 0
+    for r in r_nums:
+        if l==r:
+            l_count+=1
+    similarity_total+=l_count*l
+print(f"Part 2: {similarity_total}")
